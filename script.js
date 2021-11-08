197e0b5d2f258b83d433866db00f977dd1982c8e
nebula391 <stuffyworld@outlook.com>
2021-11-08 09:45:11 -0800

    2 files modified

--- script.js
+++ script.js
@@ -1,7 +1,54 @@
 //blank javascript file
-var c = document.getElementById('canvas');
-var ctx = c.getContext('2d');
-ctx.moveTo(0, 0);
-ctx.lineTo(200, 100);
-ctx.stroke();
-ctx.fillRect(50, 50, 20, 20);
+var canvas = document.getElementById('canvas');
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+var ctx = canvas.getContext('2d');
+
+class Player {
+  constructor(x, y, radius, color) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+  }
+  draw() {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    ctx.fillStyle = this.color;
+    ctx.fill();
+  }
+}
+
+class Projectile {
+  constructor(x, y, radius, color, velocity) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+    this.velocity = velocity;
+  }
+  draw() {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    ctx.fillStyle = this.color;
+    ctx.fill();
+  }
+}
+const centerX = canvas.width / 2;
+const centerY = canvas.height / 2;
+
+const player = new Player(centerX, centerY, 30, 'blue');
+
+player.draw();
+
+addEventListener('click', (event) => {
+  const projectile = new Projectile(
+    event.clientX,
+    event.clientY,
+    5,
+    'red',
+    null
+  );
+  projectile.draw();
+}
+);
