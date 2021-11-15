b3ba6775745adc4ab818fd311ff1914b4f928e05
nebula391 <stuffyworld@outlook.com>
2021-11-15 09:13:46 -0800

    1 file modified

--- script.js
+++ script.js
@@ -70,7 +70,7 @@ const enemies = [];
 
 function spawnEnemies() {
   setInterval(() => {
-    const radius = 30;
+    const radius = Math.random() * (30 - 4 + 4);
     let x;
     let y;
     if (Math.random() < 0.5) {
