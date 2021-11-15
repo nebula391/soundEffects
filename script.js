f13f18a172939980a3bcebbae01f199ef650daed
nebula391 <stuffyworld@outlook.com>
2021-11-14 17:18:17 -0800

    1 file modified

--- script.js
+++ script.js
@@ -39,6 +39,26 @@ class Projectile {
     this.y = this.y + this.velocity.y;
   }
 }
+class Enemy {
+  constructor(x, y, radius, color, velocity) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+    this.velocity = velocity;
+  }
+  draw() {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    ctx.fillStyle = this.color;
+    ctx.fill();
+  }
+  update() {
+    this.draw();
+    this.x = this.x + this.velocity.x;
+    this.y = this.y + this.velocity.y;
+  }
+}
 
 const centerX = canvas.width / 2;
 const centerY = canvas.height / 2;
@@ -46,7 +66,13 @@ const centerY = canvas.height / 2;
 const player = new Player(centerX, centerY, 30, 'blue');
 
 const projectiles = [];
+const enemies = [];
 
+function spawnEnemies() {
+  setInterval(() => {
+    console.log('go');
+  }, 1000);
+}
 function animate() {
   requestAnimationFrame(animate);
   ctx.clearRect(0, 0, canvas.width, canvas.height);
