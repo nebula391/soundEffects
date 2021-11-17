99139d27b3e3d645ff45e7921044e12eff653b10
nebula391 <stuffyworld@outlook.com>
2021-11-17 10:55:08 -0800

    1 file modified

--- script.js
+++ script.js
@@ -104,6 +104,8 @@ function animate() {
     Enemy.update();
     const dist = Math.hypot(Player.x - Enemy.x, Player.y - Enemy.y);
     //59:06
+    if (dist - Player.radius - Enemy.radius < 1) {
+    }
     projectiles.forEach((Projectile, projectileIndex) => {
       const dist = Math.hypot(Projectile.x - Enemy.x, Projectile.y - Enemy.y);
       if (dist - Projectile.radius - Enemy.radius < 1) {
